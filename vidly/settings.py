9709701d849baab1170b51e83c41971f2a1d933f
Michelle <michelle.roufail@outlook.com>
2023-02-22 09:06:13 -0500

    first commit

--- vidly/settings.py
+++ vidly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*#v=qsi!(%j-l%^opf@tc-qov(i2tss9*l5!w%v^-=^s8bb=l9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
